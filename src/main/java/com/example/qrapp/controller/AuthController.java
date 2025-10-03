9227998dd78bbf67d268eb3152ba56991e209508
Donato Montrone <ext.donato.montrone@revoinsurance.com>
2025-10-03 19:05:22 +0200

    feature: add errors in register-user [QR-006]

--- src/main/java/com/example/qrapp/controller/AuthController.java
+++ src/main/java/com/example/qrapp/controller/AuthController.java
@@ -2,6 +2,8 @@ package com.example.qrapp.controller;
 
 import com.example.qrapp.model.User;
 import com.example.qrapp.service.UserService;
+import jakarta.validation.Valid;
+import java.security.Principal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,108 +14,108 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.security.Principal;
-
 
 @Controller
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final UserService userService;
+  private final UserService userService;
 
-    @GetMapping("/")
-    public String home(Principal principal) {
-        if (principal == null) {
-            return "redirect:/login";
-        }
-        User user = userService.findByEmail(principal.getName()).orElse(null);
-        System.out.println(principal);
-        if (user == null) {
-            return "redirect:/login";
-        } else {
-            if (userService.isAdmin(user)) {
-                return "redirect:/admin/dashboard";
-            } else {
-                return "redirect:/user/dashboard";
-            }
-        }
+  @GetMapping("/")
+  public String home(Principal principal) {
+    if (principal == null) {
+      return "redirect:/login";
+    }
+    User user = userService.findByEmail(principal.getName()).orElse(null);
+    System.out.println(principal);
+    if (user == null) {
+      return "redirect:/login";
+    } else {
+      if (userService.isAdmin(user)) {
+        return "redirect:/admin/dashboard";
+      } else {
+        return "redirect:/user/dashboard";
+      }
+    }
+  }
+
+  @GetMapping("/login")
+  public String login(@RequestParam(value = "error", required = false) String error,
+                      @RequestParam(value = "logout", required = false) String logout,
+                      Model model) {
+
+    if (error != null) {
+      model.addAttribute("error", true);
+      model.addAttribute("errorMessage", "Credenziali non valide. Riprova.");
     }
 
-    @GetMapping("/login")
-    public String login(@RequestParam(value = "error", required = false) String error,
-                       @RequestParam(value = "logout", required = false) String logout,
-                       Model model) {
-
-        if (error != null) {
-            model.addAttribute("error", true);
-            model.addAttribute("errorMessage", "Credenziali non valide. Riprova.");
-        }
-
-        if (logout != null) {
-            model.addAttribute("logout", true);
-            model.addAttribute("logoutMessage", "Logout effettuato con successo.");
-        }
-
-        return "auth/login";
+    if (logout != null) {
+      model.addAttribute("logout", true);
+      model.addAttribute("logoutMessage", "Logout effettuato con successo.");
     }
 
-    @GetMapping("/register")
-    public String registerFormUser(Model model, Principal principal) {
+    return "auth/login";
+  }
 
-        model.addAttribute("user", new User());
-        if (principal == null) {
-            return "auth/register";
-        }
-        return "auth/register-admin";
+  @GetMapping("/register")
+  public String registerFormUser(Model model, Principal principal) {
+
+    model.addAttribute("user", new User());
+    if (principal == null) {
+      return "auth/register";
+    }
+    return "auth/register-admin";
+  }
+
+  @PostMapping("/register-user")
+  public String registerUser(@Valid @ModelAttribute User user,
+                             BindingResult bindingResult,
+                             RedirectAttributes redirectAttributes) {
+
+
+    if (!user.getPassword().equals(user.getConfirmPassword())) {
+      bindingResult.rejectValue("password", "password.mismatch",
+          "Le password non coincidono");
     }
 
-    @PostMapping("/register-user")
-    public String registerUser(@ModelAttribute User user,
-                          BindingResult result,
-                          @RequestParam String confirmPassword,
-                          RedirectAttributes redirectAttributes) {
-
-        if (result.hasErrors()) {
-            return "auth/register";
-        }
-        if (!user.getPassword().equals(confirmPassword)) {
-            result.rejectValue("password", "error.password", "Le password non coincidono");
-            return "auth/register";
-        }
-        try {
-            userService.registerUser(user.getFirstName(), user.getLastName(), 
-                                   user.getEmail(), user.getPassword());
-            redirectAttributes.addFlashAttribute("message",
-                "Registrazione completata! Puoi ora effettuare il login.");
-            return "redirect:/login";
-        } catch (RuntimeException e) {
-            result.rejectValue("email", "error.email", e.getMessage());
-            return "auth/register";
-        }
+    if (userService.existsByEmail(user.getEmail())) {
+      bindingResult.rejectValue("email", "email.exists",
+          "Questa email è già registrata");
     }
 
-    @PostMapping("/register-admin")
-    public String registerAdmin(@ModelAttribute User user,
-                           BindingResult result,
-                           @RequestParam String confirmPassword,
-                           RedirectAttributes redirectAttributes) {
-
-        if (result.hasErrors()) {
-            return "auth/register";
-        }
-        if (!user.getPassword().equals(confirmPassword)) {
-            result.rejectValue("password", "error.password", "Le password non coincidono");
-            return "redirect:/register";
-        }
-        try {
-            User newUser = userService.createAdminUser(user.getFirstName(), user.getLastName(),
-                    user.getEmail(), user.getPassword());
-            redirectAttributes.addFlashAttribute("successMessage",
-                    "Registrazione completata!" + newUser.getEmail() + " può ora effettuare il login.");
-            return "redirect:/login";
-        } catch (RuntimeException e) {
-            result.rejectValue("email", "error.email", e.getMessage());
-            return "auth/register";
-        }
+    if (bindingResult.hasErrors()) {
+      return "auth/register";
     }
+
+    userService.registerUser(user.getFirstName(), user.getLastName(),
+        user.getEmail(), user.getPassword());
+    redirectAttributes.addFlashAttribute("message",
+        "Registrazione completata! Puoi ora effettuare il login.");
+    return "redirect:/login";
+  }
+
+  @PostMapping("/register-admin")
+  public String registerAdmin(@ModelAttribute User user,
+                              BindingResult result,
+                              @RequestParam String confirmPassword,
+                              RedirectAttributes redirectAttributes) {
+
+    if (result.hasErrors()) {
+      return "auth/register";
+    }
+    if (!user.getPassword().equals(confirmPassword)) {
+      result.rejectValue("password", "error.password", "Le password non coincidono");
+      return "redirect:/register";
+    }
+    try {
+      User newUser = userService.createAdminUser(user.getFirstName(), user.getLastName(),
+          user.getEmail(), user.getPassword());
+      redirectAttributes.addFlashAttribute("successMessage",
+          "Registrazione completata!" + newUser.getEmail() + " può ora effettuare il login.");
+      return "redirect:/login";
+    } catch (RuntimeException e) {
+      result.rejectValue("email", "error.email", e.getMessage());
+      return "auth/register";
+    }
+  }
 }
--- src/main/java/com/example/qrapp/model/User.java
+++ src/main/java/com/example/qrapp/model/User.java
@@ -1,21 +1,41 @@
 package com.example.qrapp.model;
 
-import jakarta.persistence.*;
+import static com.example.qrapp.constants.Constant.ROLE_;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.*;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
-import java.time.LocalDateTime;
-import java.util.*;
-
-import static com.example.qrapp.constants.Constant.ROLE_;
 
 @Entity
 @Table(name = "users")
@@ -26,95 +46,98 @@ import static com.example.qrapp.constants.Constant.ROLE_;
 @ToString
 public class User implements UserDetails {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
 
-    @NotBlank
-    @Size(max = 50)
-    private String firstName;
+  @NotBlank(message = "Inserisci il nome")
+  @Size(min = 3, max = 20, message = "Il nome deve essere compreso tra 3 e 20 caratteri")
+  private String firstName;
 
-    @NotBlank
-    @Size(max = 50)
-    private String lastName;
+  @NotBlank(message = "Inserisci il cognome")
+  @Size(min = 3, max = 20, message = "Il cognome deve essere compreso tra 3 e 20 caratteri")
+  private String lastName;
 
-    @NotBlank
-    @Email
-    @Size(max = 100)
-    @Column(unique = true)
-    private String email;
+  @NotBlank(message = "Inserisci la mail")
+  @Email(message = "Il formato della email non è corretto")
+  @Size(min = 8, max = 30, message = "La mail deve essere compresa tra 8 e 30 caratteri")
+  @Column(unique = true)
+  private String email;
 
-    @NotBlank
-    @Size(min = 6, max = 100)
-    private String password;
+  @NotBlank(message = "Inserisci la password")
+  @Size(min = 6, max = 100, message = "La password deve avere minimo 6 caratteri")
+  private String password;
 
-    @Column(name = "created_at")
-    @CreationTimestamp
-    private LocalDateTime createdAt;
+  @Transient
+  private String confirmPassword;
 
-    @Column(name = "updated_at")
-    @UpdateTimestamp
-    private LocalDateTime updatedAt;
+  @Column(name = "created_at")
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 
-    @Column(name = "super_admin")
-    @ColumnDefault(value = "false")
-    private boolean superAdmin = false;
+  @Column(name = "updated_at")
+  @UpdateTimestamp
+  private LocalDateTime updatedAt;
 
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(
-        name = "user_roles",
-        joinColumns = @JoinColumn(name = "user_id"),
-        inverseJoinColumns = @JoinColumn(name = "role_id")
-    )
-    @ToString.Exclude
-    private Set<Role> roles = new HashSet<>();
+  @Column(name = "super_admin")
+  @ColumnDefault(value = "false")
+  private boolean superAdmin = false;
 
-    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, orphanRemoval = true)
-    @ToString.Exclude
-    private List<QrCode> qrCodes;
+  @ManyToMany(fetch = FetchType.EAGER)
+  @JoinTable(
+      name = "user_roles",
+      joinColumns = @JoinColumn(name = "user_id"),
+      inverseJoinColumns = @JoinColumn(name = "role_id")
+  )
+  @ToString.Exclude
+  private Set<Role> roles = new HashSet<>();
 
-    public User(String firstName, String lastName, String email, String password) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.password = password;
-    }
+  @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, orphanRemoval = true)
+  @ToString.Exclude
+  private List<QrCode> qrCodes;
 
-    // UserDetails implementation
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return roles.stream()
-                .map(role -> new SimpleGrantedAuthority(ROLE_ + role.getName()))
-                .toList();
-    }
+  public User(String firstName, String lastName, String email, String password) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.password = password;
+  }
 
-    @Override
-    public String getUsername() {
-        return email;
-    }
+  // UserDetails implementation
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return roles.stream()
+        .map(role -> new SimpleGrantedAuthority(ROLE_ + role.getName()))
+        .toList();
+  }
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
+  @Override
+  public String getUsername() {
+    return email;
+  }
 
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
 
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
 
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
 
-    @PreUpdate
-    private void preUpdate() {
-        this.updatedAt = LocalDateTime.now();
-    }
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+
+  @PreUpdate
+  private void preUpdate() {
+    this.updatedAt = LocalDateTime.now();
+  }
 }
